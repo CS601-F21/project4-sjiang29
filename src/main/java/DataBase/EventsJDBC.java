50a188845a1da9f4c967718b9c86fd2652cb4b92
sjiang29 <69825128+sjiang29@users.noreply.github.com>
2021-12-04 15:45:22 -0800

    update sql queries for tickets

--- src/main/java/DataBase/EventsJDBC.java
+++ src/main/java/DataBase/EventsJDBC.java
@@ -89,7 +89,6 @@ public class EventsJDBC {
             insertEventStmt.setString(7, endTime);
         }
 
-
         insertEventStmt.executeUpdate();
     }
 
--- src/main/java/DataBase/TicketsJDBC.java
+++ src/main/java/DataBase/TicketsJDBC.java
@@ -7,5 +7,23 @@ import java.sql.SQLException;
 
 public class TicketsJDBC {
 
+    public static ResultSet executeSelectTicketsByEventId(Connection con, int eventId) throws SQLException {
+        String selectTicketsByEventIdSql = "SELECT * FROM tickets WHERE sold='no' AND event_id=? ;";
+        PreparedStatement selectTicketsByEventIdStmt = con.prepareStatement(selectTicketsByEventIdSql);
+        selectTicketsByEventIdStmt.setInt(1, eventId);
+
+        ResultSet results = selectTicketsByEventIdStmt.executeQuery();
+        return results;
+    }
+
+    public static void transferTicket(Connection con, String oldBuyerEmail, String newBuyerEmail, int ticketId) throws SQLException {
+        String updateBuyerEmailSql = "UPDATE tickets SET buyer_email=? WHERE id=?; ";
+        PreparedStatement updateBuyerEmailStmt = con.prepareStatement(updateBuyerEmailSql);
+
+        updateBuyerEmailStmt.setString(1, newBuyerEmail);
+        updateBuyerEmailStmt.setInt(2, ticketId);
+        updateBuyerEmailStmt.executeUpdate();
+    }
+
 
 }
